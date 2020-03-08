ebee558beed76e05e03af25ade5c5f60a296ab28
Kitiya <kate.kitiya@gmail.com>
2020-03-08 16:07:12 -0400

    Initial commit

---
+++ src/pages/home/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HOME PAGE</h1>
+    </div>
+  );
+};
+
+export default HomePage;
